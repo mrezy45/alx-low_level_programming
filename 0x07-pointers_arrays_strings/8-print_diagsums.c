981c12cdb71239e7149130c642402bc9e6d59032
Ezra Rodney Mpiima <mrezy45@gmail.com>
2023-03-07 11:15:31 -0800

    0x07. C - Even more pointers, arrays and strings tasks

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,24 +1,28 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
- * print_diagsums - prints the sums of the two diagonals of a square matrix
- * @a: pointer to start of matrix
- * @size: width of matrix column
+ * print_diagsums - print the sum of the two
+ *                 diagonals of a square matrix
+ *                 of integers.
+ * @a: input pointer
+ * @size: size of the matrix
  *
- * Return: void
- */
+ * Return: nothing
+*/
+
 void print_diagsums(int *a, int size)
 {
-	int i, j, p, l = 0, r = 0;
+	int i, S1, S2;
 
-	for (i = 0; i < size; i++)
+	S1 = 0;
+	S2 = 0;
+
+	for (i = 0; i < (size * size); i++)
 	{
-		p = (i * size) + i;
-		l += *(a + p);
+		if (i % (size + 1) == 0)
+			S1 += a[i];
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			S2 += a[i];
 	}
-	for (j = 0; j < size; j++)
-	{
-		p = (j * size) + (size - 1 - j);
-		r += *(a + p);
-	}
-	printf("%i, %i\n", l, r);
-i}
+	printf("%d, %d\n", S1, S2);
+}
