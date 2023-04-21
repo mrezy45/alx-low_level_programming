2af5f70482717c014a16caf0e6fbbf5acc1bd05d
Ezra Rodney Mpiima <mrezy45@gmail.com>
2023-04-21 16:16:06 -0700

    0x10. C - Variadic functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
+
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separetor, const unsigned int n, ...);
+void print_strings(const char *separetor, const unsigned int n, ...);
+void print all(const char* const format, ...);
+#endif 
