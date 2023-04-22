8d83bcdd31cbc941e9291955d816176f77cdb96c
Ezra Rodney Mpiima <mrezy45@gmail.com>
2023-04-21 18:25:22 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,19 +8,19 @@
   *
   * Return: ...
   */
-
 int int_index(int *array, int size, int (*cmp)(int))
 {
-
 	int i = 0;
-	if(i < 0)
+
+	if (size > 0)
 	{
-		if(array != NULL && cmp != NULL)
+		if (array != NULL && cmp != NULL)
 		{
-			while(i < size)
+			while (i < size)
 			{
-				if(cmp(array[i]))
-					return(1);
+				if (cmp(array[i]))
+					return (i);
+
 				i++;
 			}
 		}
