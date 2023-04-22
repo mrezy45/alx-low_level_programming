a67033a42e0c43c01cad19e0816ef24e1a5e9491
Ezra Rodney Mpiima <mrezy45@gmail.com>
2023-04-21 18:13:07 -0700

    0x0F. C - Function pointerswq

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
 #include "function_pointers.h"
+
 /**
  * print_name - a function that prints a name.
  * @name: the name given
  * @f: function of name
  *
- * Return: void
+ * Rteurn: void
  */
-
-void print_name(char *name, void (*f) (char))
-{	
+void print_name(char *name, void (*f)(char *))
+{
 	if (name != NULL && f != NULL)
+
 		f(name);
 }
-		
